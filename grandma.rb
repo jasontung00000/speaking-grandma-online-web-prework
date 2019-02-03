fa70e9b79ead2ed42d7644ca1866522ada91b9bf
Jason Tung <jasontung123@yahoo.com>
2019-02-03 20:59:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,4 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma('I LOVE YOU GRANDMA')
+def speak_to_grandma
+  if 'I LOVE YOU GRANDMA!'
+    
