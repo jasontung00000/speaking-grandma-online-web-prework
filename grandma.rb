07072bba1d32338d7c79b5d9c1289ff5206c34b3
Jason Tung <jasontung123@yahoo.com>
2019-02-03 21:04:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 speak_to_grandma ==  'I LOVE YOU GRANDMA!'
-def speak_to_grandma
+
   if speak_to_grandma == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
   else 'HUH?! SPEAK UP, SONNY!'
